e7872760da6ba9ff6742ceac5bda2a2288ce7f34
Ashley Norman <ashley.ansell@gmail.com>
2021-08-26 09:00:23 -0400

    specify daycare vs boarding hours

--- src/components/Hours.tsx
+++ src/components/Hours.tsx
@@ -4,12 +4,23 @@ import * as React from "react"
 const Hours = () => {
   return (
     <>
-      <Typography variant="overline">Monday - Friday</Typography>
+      <Typography variant="overline" component="div">
+        Monday - Friday
+      </Typography>
+      <Typography variant="caption">Daycare & Boarding</Typography>
       <Typography>7:00 am - 12:30 pm</Typography>
       <Typography gutterBottom>3:30 pm - 7:00 pm</Typography>
-      <Typography variant="overline">Saturday</Typography>
+
+      <Typography variant="overline" component="div">
+        Saturday
+      </Typography>
+      <Typography variant="caption">Boarding Only</Typography>
       <Typography gutterBottom>9:00 am - 12:30 pm</Typography>
-      <Typography variant="overline">Sunday</Typography>
+
+      <Typography variant="overline" component="div">
+        Sunday
+      </Typography>
+      <Typography variant="caption">Boarding Only</Typography>
       <Typography gutterBottom>3:30 pm - 7:00 pm</Typography>
     </>
   )
