a4ec8a327773c7a03f9ca03c3cdd53e261aebd04
Ashley Norman <ashley.ansell@gmail.com>
2025-03-31 20:53:41 -0400

    feat: update hours and info

--- src/components/Hours.tsx
+++ src/components/Hours.tsx
@@ -14,13 +14,15 @@ const Hours = () => {
         Saturday
       </Typography>
       <Typography variant="caption">Boarding Only</Typography>
-      <Typography gutterBottom>7:00 am - 12:00 pm</Typography>
+      <Typography>7:00 am - 12:00 pm</Typography>
+      <Typography gutterBottom>3:00 pm - 6:00 pm</Typography>
 
       <Typography variant="overline" component="div">
         Sunday
       </Typography>
       <Typography variant="caption">Boarding Only</Typography>
-      <Typography gutterBottom>3:30 pm - 7:00 pm</Typography>
+      <Typography>7:00 am - 12:00 pm</Typography>
+      <Typography gutterBottom>3:00 pm - 7:00 pm</Typography>
     </>
   )
 }
